6a18f4c56d9bbcb08c6f492eec8c62bd1f23d7ca
Joni Agüero <aguerojonathanivan@gmail.com>
2018-12-15 11:57:21 -0300

    Highlights

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/Header/Header';
 import Fearured from './components/Featured/Fearured';
 import Info from './components/Info/Info';
+import Highlights from './components/Highlights/Highlights';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <Header/>
         <Fearured/>
         <Info/>
+        <Highlights/>
       </div>
     );
   }
